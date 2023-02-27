523df5e649ab63cbbe4ee83680ca3a1a7827fdd8
G4lile00 <vitor.vicente2023@gmail.com>
2023-02-27 18:10:15 -0300

    finalizado

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-const firebaseConfig = process.env.FIREBASE;
+const firebaseConfig = {
+  apiKey: "AIzaSyBbS-3zgsAvz1iW-CG6teB1DMtAMPAGVMY",
+
+  authDomain: "todo-list-2417c.firebaseapp.com",
+
+  projectId: "todo-list-2417c",
+
+  storageBucket: "todo-list-2417c.appspot.com",
+
+  messagingSenderId: "117683632709",
+
+  appId: "1:117683632709:web:d0d09a432b58ad9235012c",
+
+  measurementId: "G-XJQX226VLT",
+};
 
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
