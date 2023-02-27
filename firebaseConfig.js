31f08498813bc39856b94bf48135f40961abeba3
G4lile00 <vitor.vicente2023@gmail.com>
2023-02-27 17:55:50 -0300

    integrado completamnete com o firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyBbS-3zgsAvz1iW-CG6teB1DMtAMPAGVMY",
-
-  authDomain: "todo-list-2417c.firebaseapp.com",
-
-  projectId: "todo-list-2417c",
-
-  storageBucket: "todo-list-2417c.appspot.com",
-
-  messagingSenderId: "117683632709",
-
-  appId: "1:117683632709:web:d0d09a432b58ad9235012c",
-
-  measurementId: "G-XJQX226VLT",
-};
+const firebaseConfig = process.env.FIREBASE;
 
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
