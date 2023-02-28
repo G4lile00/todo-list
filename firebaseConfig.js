71cab69302a606c4e37b736f8caf5b5d22271b06
G4lile00 <vitor.vicente2023@gmail.com>
2023-02-28 13:20:32 -0300

    ajuste de seguranca

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBbS-3zgsAvz1iW-CG6teB1DMtAMPAGVMY",
-
-  authDomain: "todo-list-2417c.firebaseapp.com",
-
-  projectId: "todo-list-2417c",
-
-  storageBucket: "todo-list-2417c.appspot.com",
-
-  messagingSenderId: "117683632709",
-
-  appId: "1:117683632709:web:d0d09a432b58ad9235012c",
-
-  measurementId: "G-XJQX226VLT",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MSID,
+  appId: process.env.APPID,
+  measurementId: process.env.MID,
 };
 
 const app = initializeApp(firebaseConfig);
